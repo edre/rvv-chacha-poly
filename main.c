cd8753aea1ce3c3f0e836aa92195874378e2cba7
Eric Roshan-Eisner <1291499+edre@users.noreply.github.com>
2020-02-13 14:51:57 -0800

    riscv: random data poly1305 single-block test

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -58,40 +59,62 @@ void test_chacha() {
   }
 }
 
-void test_poly() {
+bool test_poly(const uint8_t* data, size_t len, const uint8_t key[32], bool verbose) {
   extern uint64_t vector_poly1305(const uint8_t* in, size_t len,
 				  const uint8_t key[32], uint8_t sig[16]);
-
-  const uint8_t zero[32] ={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  const uint8_t one[32] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  const uint8_t key[32] = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 255,
-  			   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  const uint8_t data[32] = "Setec astronomy;too many secrets";
 
   poly1305_state state;
   boring_poly1305_init(&state, key);
   boring_poly1305_update(&state, data, 16);
   uint8_t *sig = malloc(16); // gets corrupted if I define it on the stack?
   boring_poly1305_finish(&state, sig);
-  printf("boring mac: ");
-  println_hex(sig, 16);
-
   uint8_t *sig2 = malloc(16);
   vector_poly1305(data, 16, key, sig2);
-  printf("vector mac: ");
-  println_hex(sig2, 16);
 
-  if (memcmp(sig, sig2, 16)) {
-    printf("poly FAIL\n");
-  } else {
-    printf("poly PASS\n");
+  bool pass = memcmp(sig, sig2, 16) == 0;
+
+  if (verbose || !pass) {
+    printf("boring mac: ");
+    println_hex(sig, 16);
+    printf("vector mac: ");
+    println_hex(sig2, 16);
   }
+
+  free(sig);
+  free(sig2);
+  return pass;
+}
+
+void test_polys() {
+  const uint8_t zero[32] ={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  const uint8_t one[32] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  const uint8_t key[32] = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 255,
+  			   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  const uint8_t data[32] = "Setec astronomy;too many secrets";
+  bool pass = test_poly(data, 16, key, true);
+
+  // random test
+  FILE* f = fopen("/dev/urandom", "r");
+  for (int i = 0; i < 100; i++) {
+    const int len = 16;
+    fread((uint8_t*)key, 32, 1, f);
+    fread((uint8_t*)data, len, 1, f);
+    pass = pass && test_poly(data, len, key, false);
+  }
+  fclose(f);
+
+  if (pass) {
+    printf("poly PASS\n");
+  } else {
+    printf("poly FAIL\n");
+  }
+
 }
 
 int main(int argc, uint8_t *argv[]) {
   extern uint32_t vlmax_u32();
-  printf("VLMAX in blocks: %d\n", vlmax_u32());
+  //printf("VLMAX in blocks: %d\n", vlmax_u32());
   //test_chacha();
   //printf("\n");
-  test_poly();
+  test_polys();
 }
