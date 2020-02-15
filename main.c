0c9ad74beaf713aa716838620adb7bd3b9ac3f70
Eric Roshan-Eisner <edre@google.com>
2020-02-14 22:21:41 -0800

    riscv: add poly1305 instrumentation
    boring mac: 25d4926a53bb480da228ec61e0a31a38
    inst_count=12729, inst/byte=12.43
    vector mac: 25d4926a53bb480da228ec61e0a31a38
    precomputation=652, processing=437, inst/byte=0.43

--- main.c
+++ main.c
@@ -42,7 +42,7 @@ void test_chacha() {
   uint64_t end = instruction_counter();
   printf("golden: ");
   println_hex(golden, 32);
-  printf("instruction count: %d\n", end-start);
+  printf("inst_count=%d, inst/byte=%.02f\n", end - start, (float)(end - start)/len);
 
   uint8_t vector[len];
   start = instruction_counter();
@@ -50,7 +50,7 @@ void test_chacha() {
   end = instruction_counter();
   printf("vector: ");
   println_hex(vector, 32);
-  printf("instruction count: %d\n", end-start);
+  printf("inst_count=%d, inst/byte=%.02f\n", end - start, (float)(end - start)/len);
 
   if (memcmp(golden, vector, len)) {
     printf("chacha FAIL\n");
@@ -64,20 +64,29 @@ bool test_poly(const uint8_t* data, size_t len, const uint8_t key[32], bool verb
 				  const uint8_t key[32], uint8_t sig[16]);
 
   poly1305_state state;
+  uint8_t *sig = malloc(16); // gets corrupted if I define it on the stack?
+  uint64_t start = instruction_counter();
   boring_poly1305_init(&state, key);
   boring_poly1305_update(&state, data, len);
-  uint8_t *sig = malloc(16); // gets corrupted if I define it on the stack?
   boring_poly1305_finish(&state, sig);
+  uint64_t end = instruction_counter();
+  uint64_t boring_count = end - start;
+
   uint8_t *sig2 = malloc(16);
-  vector_poly1305(data, len, key, sig2);
+  start = instruction_counter();
+  uint64_t mid = vector_poly1305(data, len, key, sig2);
+  end = instruction_counter();
 
   bool pass = memcmp(sig, sig2, 16) == 0;
 
   if (verbose || !pass) {
     printf("boring mac: ");
     println_hex(sig, 16);
+    printf("inst_count=%d, inst/byte=%.02f\n", boring_count, (float)(boring_count)/len);
     printf("vector mac: ");
     println_hex(sig2, 16);
+    printf("precomputation=%d, processing=%d, inst/byte=%.02f\n",
+	   mid - start, end - mid, (float)(end - mid)/len);
   }
 
   free(sig);
@@ -86,14 +95,15 @@ bool test_poly(const uint8_t* data, size_t len, const uint8_t key[32], bool verb
 }
 
 void test_polys() {
+  const int big_len = 1024;
   uint8_t *zero = malloc(2000);
-  uint8_t *max_bits = malloc(2000);
-  memset(max_bits, 2000, 0xff);
+  uint8_t *max_bits = malloc(big_len);
+  memset(max_bits, 0xff, big_len);
   const uint8_t one[32] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
   const uint8_t key[32] = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 255,
   			   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
   const uint8_t data[272] = "Setec astronomy;too many secrets";
-  bool pass = test_poly(data, 272, key, true);
+  bool pass = test_poly(max_bits, big_len, max_bits, true);
 
   if (!pass)
     goto end;
@@ -127,8 +137,8 @@ void test_polys() {
 
 int main(int argc, uint8_t *argv[]) {
   extern uint32_t vlmax_u32();
-  //printf("VLMAX in blocks: %d\n", vlmax_u32());
-  //test_chacha();
-  //printf("\n");
+  printf("VLMAX in blocks: %d\n", vlmax_u32());
+  test_chacha();
+  printf("\n");
   test_polys();
 }
